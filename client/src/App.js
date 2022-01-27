cc678209fa1f7e48cab453f6604948c79647001e
Sejo Basic <sejoob@gmail.com>
2022-01-26 21:28:32 -0700

    update1

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,9 @@ import Card from "./Card";
 
 function App() {
   const [items, setItems] = useState([]);
+  // const [sortItem, setSort] = useState([]);
+
+  // # get all food items
   useEffect(() => {
     fetch("http://localhost:9292/items")
       .then((r) => r.json())
@@ -16,6 +19,35 @@ function App() {
       .then(console.log(items));
     // .then((items) => console.log(items));
   }, []);
+
+  // function sortItem(item) {
+  //   fetch("http://localhost:9292/items/by_date")
+  //   .then((r) => r.json())
+  //   .then(setSort)
+  //   .then(console.log(items));
+  // } 
+
+    //Creates a new item
+  function postItem(item) {
+    fetch(
+      //TODO: Write a post
+    )
+      .then(res => res.json())
+      .then(newItem => {
+        setItems([newItem,...items])
+      })
+    }
+
+    //Deletes item
+  function handleDelete(id) {
+    fetch(`http://localhost:9292/items/${id}`, {
+      method: 'DELETE'
+    })
+    .then(res => res.json())
+    .then(data => {
+      setItems(items.filter(i => i.id !== id))
+    })
+  }
 
   return (
     <div className="App">
@@ -32,10 +64,10 @@ function App() {
             <Receipts />
           </Route> */}
           <Route path="/Receipts">
-            <Card items={items} />
+          {items.map(i => <Card item={i} handleDelete={handleDelete} key={`${i.id}${i.item_name}`}/>)}
           </Route>
           <Route path="/Groceries">
-            <Groceries />
+            <Groceries postItem={postItem} />
           </Route>
         </Switch>
       </BrowserRouter>
@@ -44,3 +76,4 @@ function App() {
 }
 
 export default App;
+
--- client/src/Card.js
+++ client/src/Card.js
@@ -1,24 +1,23 @@
 import React from "react";
 
-function Card({ items }) {
+function Card({ item, handleDelete  }) {
+  
   // const pic =   {items[0].image}
   return (
+    
     <div className="item_card">
-      <h1>Item</h1>
-      {/* {console.log(items[3].image)} */}
+      <img alt="none loaded :(" src={`${item.image}`} className="item_image" style={{width:"20%"}}/>
+      <h1 className="item_title"> {item.item_name} </h1>
+      <p className="item_expiration_date"> Purchase Date: {item.purchase_date} </p>
+      <p className="item_expiration_date"> Expiration Date: {item.expiry_date} </p>
+      
+      <button onClick={() => handleDelete(item.id)}>Delete Item</button>
 
-      {/* server/db/Pictures/carrots.png */}
-      <img
-        alt="none loaded :("
-        src={`${items[3].image}`}
-        className="item_image"
-      />
-
-      {/* DELETE BUTTON */}
-      <span className="item_title"> {items[0].item_name} </span>
-      <span className="item_expiration_date"> {items[0].expiry_date} </span>
     </div>
   );
 }
 
 export default Card;
+
+
+
