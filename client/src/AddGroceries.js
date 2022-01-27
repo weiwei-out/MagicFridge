2e4eed39fba7130a9e9a67c1e47e0dde65a7960d
Andrew Wei <andrewwei913@gmail.com>
2022-01-27 08:49:40 -0800

    CSS 02

--- client/src/AddGroceries.js
+++ client/src/AddGroceries.js
@@ -1,54 +1,52 @@
-import React,{useState} from "react";
-import Card from "./Card"
+import React, { useState } from "react";
+import Card from "./Card";
 // import Card from "./Card";
 
-function AddGroceries({postItem}) {
-const [newitem,setNewitem] = useState({
-   item_name:"",
-   expiry_date:"",
-   purchase_date:"",
-    quantity:0,
-    item_type:"",
-    image:""
+function AddGroceries({ postItem }) {
+  const [newitem, setNewitem] = useState({
+    item_name: "",
+    expiry_date: "",
+    purchase_date: "",
+    quantity: 0,
+    item_type: "",
+    image: "",
     //fridge_id:fridge1.id
-  })
+  });
 
+  function handleChange(event) {
+    const key = event.target.name;
+    const value = event.target.value;
+    setNewitem({ ...newitem, [key]: value });
+  }
 
-function handleChange(event){
-  const key = event.target.name
-  const value = event.target.value
-  setNewitem({...newitem, [key]:value})
-}
-
-function handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
-    postItem(newitem)
-
+    postItem(newitem);
   }
 
   return (
-    <div>
+    <div id="groceries">
       <h1> Stock my fridge! </h1>
       <h4> Put your groceries in your fridge</h4>
 
       <form onSubmit={handleSubmit}>
-      <label >Name:</label>
-        <input type="text" name="item_name" onChange={handleChange}/>
+        <label>Name:</label>
+        <input type="text" name="item_name" onChange={handleChange} />
         <p></p>
-        <label >Purchase Date(mon/day/year):</label>
-        <input type="text" name="purchase_date" onChange={handleChange}/>
+        <label>Purchase Date(mon/day/year):</label>
+        <input type="text" name="purchase_date" onChange={handleChange} />
         <p></p>
-      <label >Expirition.Date(mon/day/year):</label>
-        <input type="text" name="expiry_date" onChange={handleChange}/>
+        <label>Expirition.Date(mon/day/year):</label>
+        <input type="text" name="expiry_date" onChange={handleChange} />
         <p></p>
-      <label >Type:</label>
-        <input type="text" name="item_type" onChange={handleChange}/>
+        <label>Type:</label>
+        <input type="text" name="item_type" onChange={handleChange} />
         <p></p>
-      <label >Quantity:</label>
+        <label>Quantity:</label>
         <input type="number" name="quantity" onChange={handleChange} />
         <p></p>
-        <label >Image:</label>
-        <input type="text" name="image" onChange={handleChange}/>
+        <label>Image:</label>
+        <input type="text" name="image" onChange={handleChange} />
         <p></p>
         <button type="submit">create</button>
       </form>
--- client/src/Card.js
+++ client/src/Card.js
@@ -1,23 +1,30 @@
 import React from "react";
 
-function Card({ item, handleDelete  }) {
-  
+function Card({ item, handleDelete }) {
   // const pic =   {items[0].image}
   return (
-    
     <div className="item_card">
-      <img alt="none loaded :(" src={`${item.image}`} className="item_image" style={{width:"20%"}}/>
+      <img
+        alt="none loaded :("
+        src={`${item.image}`}
+        className="item_image"
+        // style={{ width: "20%" }}
+      />
       <h1 className="item_title"> {item.item_name} </h1>
-      <p className="item_expiration_date"> Purchase Date: {item.purchase_date} </p>
-      <p className="item_expiration_date"> Expiration Date: {item.expiry_date} </p>
-      
-      <button onClick={() => handleDelete(item.id)}>Delete Item</button>
+      <p className="item_expiration_date">
+        {" "}
+        Purchase Date: {item.purchase_date}{" "}
+      </p>
+      <p className="item_expiration_date">
+        {" "}
+        Expiration Date: {item.expiry_date}{" "}
+      </p>
 
+      <button className="delete" onClick={() => handleDelete(item.id)}>
+        Delete Item
+      </button>
     </div>
   );
 }
 
 export default Card;
-
-
-
